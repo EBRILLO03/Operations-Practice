6b8b3cbed8dc2bafca61f7433aa26746ee57e870
EBRILLO03 <42975610+EBRILLO03@users.noreply.github.com>
2018-09-28 12:30:08 -0400

    Add files via upload

---
+++ src/OperationsTest.java
@@ -0,0 +1,22 @@
+
+public class OperationsTest {
+
+	public static void main(String[] args) {
+			int mystery;
+			mystery=5;
+			System.out.println("Default Value: " + mystery);
+			mystery= mystery + 5;
+			System.out.println("Add 5: " + mystery);
+			mystery= mystery/2; 
+			System.out.println("Divide by 2: " + mystery);
+			mystery= mystery *4;
+			System.out.println("Multiply by 4: " + mystery);
+			mystery= mystery-6;
+			System.out.println("Subtract 6: " + mystery);
+			mystery= mystery%2;
+		System.out.println("Modularily Divide by 2: " + mystery);
+					
+					
+	}
+
+}
